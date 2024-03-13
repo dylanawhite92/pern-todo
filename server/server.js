b85a5aae309570a5ab3995a1b01d4b6ea753cbe9
The Crystal King <dylanawhite11@gmail.com>
2024-03-13 11:28:23 -0500

    reverted back to just port 5000 for now because it's unnecessary at this point to break port out into variable and set up process env for it yet

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 5000;
 
 // Middleware - Get access to request.body object for client side data
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000');
 });
