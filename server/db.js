c2a4f51603379f7f0d29b8c6d7bd09f8c0b53fda
The Crystal King <dylanawhite11@gmail.com>
2024-03-13 11:23:28 -0500

    modified db.js to connect to postgres db

--- server/db.js
+++ server/db.js
@@ -5,3 +5,13 @@ const local_pw = process.env.PSQL_PASSWORD;
 
 // postgres connection with pg NPM package
 const Pool = require('pg').Pool;
+
+const pool = new Pool({
+  user: 'postgres',
+  password: local_pw,
+  host: 'localhost',
+  port: 5432,
+  database: 'pern_todo_db',
+});
+
+module.exports = pool;
