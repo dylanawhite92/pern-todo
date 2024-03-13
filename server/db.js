16391a9cc730e1d29e55aa148b1fa1e72b64f491
The Crystal King <dylanawhite11@gmail.com>
2024-03-13 11:19:47 -0500

    testing dot env

--- server/db.js
+++ server/db.js
@@ -1,2 +1,7 @@
+// Hide local password
+require('dotenv').config();
+
+const local_pw = process.env.PSQL_PASSWORD;
+
 // postgres connection with pg NPM package
 const Pool = require('pg').Pool;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const port = 5000;
 
 // Middleware - Get access to request.body object for client side data
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
