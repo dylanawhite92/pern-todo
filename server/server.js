fc487ae95c99ac98385836a4faeb4a9f5760d108
The Crystal King <dylanawhite11@gmail.com>
2024-03-13 11:30:08 -0500

    framed out routes with comments, will build next

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 // Middleware - Get access to request.body object for client side data
 app.use(cors());
 app.use(express.json());
 
+// SET ROUTES
+
+// Create a to-do
+
+// Get all to-dos
+
+// Get a to-do
+
+// Update a to-do
+
+// Delete a to-do
+
+// Set Port
 app.listen(5000, () => {
   console.log('Server is listening on port 5000');
 });
