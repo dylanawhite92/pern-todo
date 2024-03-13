80edaa3ad9d0e2feff7cf6110cce5b72b233883f
The Crystal King <dylanawhite11@gmail.com>
2024-03-13 10:13:59 -0500

    middleware added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
+
+// Middleware - Get access to request.body object for client side data
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000');
