677f706e43c14161a55c43a4886bf4baff9e41ec
The Crystal King <dylanawhite11@gmail.com>
2024-03-18 11:36:09 -0500

    importing table

--- client/src/components/listToDos.js
+++ client/src/components/listToDos.js
@@ -1,5 +1,26 @@
 const ListToDos = () => {
-  return <h1>List To-Dos</h1>;
+  return (
+    <>
+      <table class='table mt-5 text-center'>
+        <thead>
+          <tr>
+            <th scope='col'>Description</th>
+            <th scope='col'>Edit</th>
+            <th scope='col'>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Keeping for reference */}
+          {/* <tr>
+            <th scope='row'>1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr> */}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default ListToDos;
