d119ffac03e4b624eabc6b69d260e8266f862539
The Crystal King <dylanawhite11@gmail.com>
2024-03-18 11:39:55 -0500

    receiving data from db in console currently

--- client/src/components/listToDos.js
+++ client/src/components/listToDos.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from 'react';
+
 const ListToDos = () => {
+  // Make fetch request every time component is rendered
+  const getToDos = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/todos');
+      const jsonData = await response.json();
+
+      console.log(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getToDos();
+  });
+
   return (
     <>
       <table class='table mt-5 text-center'>
